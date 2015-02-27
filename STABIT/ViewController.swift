482ec0c5fc014c33cbe99cc305248dcdc200946c
jessnev4life <jessnev4evr@gmail.com>
2015-02-26 16:04:13 -0800

    Initial Commit

---
+++ STABIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  STABIT
+//
+//  Created by Maria Lopez on 2/26/15.
+//  Copyright (c) 2015 Jessica Tercero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
